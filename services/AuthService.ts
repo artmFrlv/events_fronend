516bbe67dd13fc879f3eb852f77bd31d6f54633f
ArtemFrolv <papich2648@gmail.com>
2021-10-17 22:31:17 +0300

    test

--- services/AuthService.ts
+++ services/AuthService.ts
@@ -6,18 +6,18 @@ import {IUser} from '@models/IUser';
 
 export default class AuthService {
     static async login(login: string, password: string): Promise<AxiosResponse<IUser>> {
-        return $api.post('/auth/login', {login, password});
+        return $api.post('http://84.38.183.53:5000/api/auth/login', {login, password});
     }
 
     static async registration(registrationRequest: RegistrationRequest): Promise<AxiosResponse<IUser>> {
-        return $api.post('/auth/registration', {...registrationRequest});
+        return $api.post('http://84.38.183.53:5000/api/auth/registration', {...registrationRequest});
     }
 
     static async logout(): Promise<AxiosResponse<void>> {
-        return $api.get('/auth/logout');
+        return $api.get('http://84.38.183.53:5000/api/auth/logout');
     }
 
     static async checkAuth(): Promise<AxiosResponse<IUser>> {
-        return $api.get('/auth/refresh').catch((e) => {throw e});
+        return $api.get('http://84.38.183.53:5000/api/auth/refresh').catch((e) => {throw e});
     }
 }
