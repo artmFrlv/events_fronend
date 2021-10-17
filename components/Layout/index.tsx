17ccaa38283a49b196431bbe78a2320cd63333ef
ArtemFrolv <papich2648@gmail.com>
2021-10-17 19:06:35 +0300

    add meta

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,6 +2,7 @@ import React, {FC, ReactNode} from 'react';
 import styles from './Layout.module.css';
 import Navbar from '@components/Navbar';
 import {useTypedSelector} from '@hooks/useTypedSelector';
+import Head from 'next/head';
 
 interface LayoutProps {
     children: ReactNode;
@@ -11,6 +12,10 @@ const Layout: FC<LayoutProps> = ({children}) => {
     const errors = useTypedSelector(state => state.auth.errors);
     return (
         <div className={styles.layout}>
+            <Head>
+                <title>Создавай встречи</title>
+                <meta httpEquiv={'Content-Security-Policy'} content={'upgrade-insecure-requests'} />
+            </Head>
             <Navbar />
             <div className={styles.content}>
                 {children}
